8709a8adc9fe9599958faeeb0448c40567474747
Ko Ko Lin Oo <kokolin.oo@kbzbank.com>
2025-07-06 23:41:00 +0630

    git credential

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/koko-linoo/go-fiber-api/config"
 	"github.com/koko-linoo/go-fiber-api/routes"
@@ -16,4 +18,6 @@ func main() {
 	routes.InitRoutes(api)
 
 	app.Listen(":3000")
+
+	log.Println("Server started on port :3000")
 }
